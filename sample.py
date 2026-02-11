80397b2fa817013e7bac1e5386043b48f314f2d2
Erkhemee <ericwasepic127@outlook.com>
2026-02-11 13:42:51 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -22,14 +22,14 @@ if __name__ == '__main__':
   run = "Y"
   while run == "Y":
     code = input("Item to encode: ")
-    key = int(input("Key: ")
+    key = int(input("Key: "))
     print(encoder(code, key))
     run = input("Want to run again (Y/n)? ")
 
   runs = "Y"
   while runs == "Y":
     code = input("Item to decode: ")
-    key = int(input("Key: ")
+    key = int(input("Key: "))
     print(decoder(code, key))
     runs = input("Want to run again (Y/n)? ")
 
